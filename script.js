874ccf8eebc904c4da8fb39a42544933ed6634c3
nerminDonlag <nermin.donlag@gmail.com>
2022-03-07 18:56:13 +0100

    Display winner

--- script.js
+++ script.js
@@ -16,60 +16,67 @@ cells.forEach((cell) => {
     cell.style.backgroundColor = 'black';
     cell.innerHTML = `${activePlayer}`;
     cell.classList.add(`${activePlayer.toLocaleLowerCase()}`);
+    checkWinner();
     activePlayer = switchPlayer(activePlayer);
     cell.removeEventListener('click', playCell);
-    checkWinner();
   });
 });
+
+function setWinner() {
+  document.querySelector('.message > p')
+      .innerHTML = `${activePlayer} won!`;
+  document.querySelector('.message')
+      .style.display = 'flex';
+}
 
 function checkWinner() {
   if (cells[0].classList.contains('x')) {
     if (cells[1].classList.contains('x')) {
       if (cells[2].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
     if (cells[3].classList.contains('x')) {
       if (cells[6].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
     if (cells[4].classList.contains('x')) {
       if (cells[8].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
   }
   if (cells[1].classList.contains('x')) {
     if (cells[4].classList.contains('x')) {
       if (cells[7].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
   }
   if (cells[2].classList.contains('x')) {
     if (cells[5].classList.contains('x')) {
       if (cells[8].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
   }
   if (cells[3].classList.contains('x')) {
     if (cells[4].classList.contains('x')) {
       if (cells[5].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
   }
   if (cells[6].classList.contains('x')) {
     if (cells[7].classList.contains('x')) {
       if (cells[8].classList.contains('x')) {
-        alert('x');
+        setWinner();
       }
     }
     if(cells[4].classList.contains('x')) {
       if(cells[2].classList.contains('x')){
-        alert('x');
+        setWinner();
       }
     }
   }
@@ -77,50 +84,50 @@ function checkWinner() {
   if (cells[0].classList.contains('o')) {
     if (cells[1].classList.contains('o')) {
       if (cells[2].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
     if (cells[3].classList.contains('o')) {
       if (cells[6].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
     if (cells[4].classList.contains('o')) {
       if (cells[8].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
   }
   if (cells[1].classList.contains('o')) {
     if (cells[4].classList.contains('o')) {
       if (cells[7].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
   }
   if (cells[2].classList.contains('o')) {
     if (cells[5].classList.contains('o')) {
       if (cells[8].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
   }
   if (cells[3].classList.contains('o')) {
     if (cells[4].classList.contains('o')) {
       if (cells[5].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
   }
   if (cells[6].classList.contains('o')) {
     if (cells[7].classList.contains('o')) {
       if (cells[8].classList.contains('o')) {
-        alert('o');
+        setWinner();
       }
     }
     if(cells[4].classList.contains('o')) {
       if(cells[2].classList.contains('o')){
-        alert('o');
+        setWinner();
       }
     }
   }
