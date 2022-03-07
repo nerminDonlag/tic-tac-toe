6e9310afeb7abc7f0e2a5a4173cf8b3266b18f6b
nerminDonlag <nermin.donlag@gmail.com>
2022-03-07 16:24:45 +0100

    Switch player on click

---
+++ script.js
@@ -0,0 +1,20 @@
+let activePlayer = 'X';
+
+function switchPlayer(player) {
+  let currentPlayer = player;
+  if(currentPlayer == 'X') {
+    currentPlayer = 'O';
+  } else {
+    currentPlayer = 'X';
+  }
+  return currentPlayer;
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+  cell.addEventListener('click', function playCell() {
+    cell.style.backgroundColor = 'black';
+    activePlayer = switchPlayer(activePlayer);
+    cell.removeEventListener('click', playCell);
+  });
+});
